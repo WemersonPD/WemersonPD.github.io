c569992cf5c08cb495caf620fc05a41b1df6c3b0
WemersonPD <wpdads10@gmail.com>
2020-04-21 03:35:37 -0300

    Terminado 1 - Falta refatorar HTML, e rotas

---
+++ src/js/toggle.js
@@ -0,0 +1,10 @@
+const navBarMobile = document.querySelector('.navBar')
+const toggle = document.querySelector('.toggle')
+const links = document.querySelector('[clicando]')
+
+function toggleClick() {
+    navBarMobile.appendChild(links)
+    links.setAttribute('display', 'block')
+}
+
+module.export = toggleClick
